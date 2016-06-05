e0956ce1bea24c00b8c5ba25f07b065ff096d88c
RODIEL <Rodielmj@gmail.com>
2016-06-05 17:40:20 -0400

    cambios nuevos

--- app/src/main/java/com/proyecto/ubicua/apecapp/LoginActivity.java
+++ app/src/main/java/com/proyecto/ubicua/apecapp/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.proyecto.ubicua.apecapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+
+    public void LoginOnClick(View view){
+        Intent Home = new Intent (LoginActivity.this,HomeActivity.class);
+        LoginActivity.this.startActivity(Home);
+
+
     }
 }
