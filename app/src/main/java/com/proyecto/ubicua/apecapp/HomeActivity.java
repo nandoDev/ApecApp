6ba30b4410fecb91de12ceb64eb427d1797706f5
Hernando Delgado <hernando.delgado01@gmail.com>
2016-06-06 20:43:32 -0300

    Ultimo cambio verificando pull Rodiel

--- app/src/main/java/com/proyecto/ubicua/apecapp/HomeActivity.java
+++ app/src/main/java/com/proyecto/ubicua/apecapp/HomeActivity.java
@@ -47,7 +47,7 @@ public class HomeActivity extends AppCompatActivity {
             }
         }
     }
-
+    //hola
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
